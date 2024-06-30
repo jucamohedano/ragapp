c99677e48cfb3a73dcc6622aa20589cab6cc7ef8
Juan Camacho Mohedano <jucamohedano@gmail.com>
2024-06-30 17:36:59 +0200

    comment out old static mount used in dev env

--- main.py
+++ main.py
@@ -69,7 +69,8 @@ app.mount(
     "",
     StaticFiles(directory="static", check_dir=False, html=True),
 )
-app.mount("/api/data", StaticFiles(directory="data"), name="static")
+
+# app.mount("/api/data", StaticFiles(directory="data"), name="static")
 # app.mount("", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
